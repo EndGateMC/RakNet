bd5679b78cb4192c71633f116479a0652ec37d84
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-06-03 05:10:17 +0800

    refactor: optimize DomainNameToIP_Berkley_IPV4And6

--- src/RakNetSocket2_Berkley_NativeClient.cpp
+++ src/RakNetSocket2_Berkley_NativeClient.cpp
@@ -30,42 +30,47 @@
 
 void DomainNameToIP_Berkley_IPV4And6(const char* domainName, char ip[65]) {
 #if RAKNET_SUPPORT_IPV6 == 1
-    struct addrinfo hints, *res, *p;
+    // 确保ip数组被正确初始化
+    const size_t IP_BUFFER_SIZE = 65;
+    memset(ip, 0, IP_BUFFER_SIZE);
+
+    if (domainName == nullptr || domainName[0] == '\0') { return; }
+
+    struct addrinfo hints, *res = nullptr, *p = nullptr;
     int             status;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family   = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = AF_UNSPEC; // 支持IPv4和IPv6
     hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags    = AI_ADDRCONFIG; // 只返回当前系统支持的地址类型
 
-    if ((status = getaddrinfo(domainName, NULL, &hints, &res)) != 0) {
-        memset(ip, 0, sizeof(ip));
-        return;
+    status = getaddrinfo(domainName, NULL, &hints, &res);
+    if (status != 0 || res == nullptr) { return; }
+
+    // 遍历所有返回的地址
+    for (p = res; p != nullptr; p = p->ai_next) {
+        if (p->ai_family == AF_INET) {
+            // IPv4处理
+            struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
+            inet_ntop(AF_INET, &(ipv4->sin_addr), ip, IP_BUFFER_SIZE);
+            break; // 使用第一个IPv4地址
+        }
+#if RAKNET_SUPPORT_IPV6 == 1
+        else if (p->ai_family == AF_INET6) {
+            // IPv6处理
+            struct sockaddr_in6* ipv6 = (struct sockaddr_in6*)p->ai_addr;
+            inet_ntop(AF_INET6, &(ipv6->sin6_addr), ip, IP_BUFFER_SIZE);
+            break; // 使用第一个IPv6地址
+        }
+#endif
     }
 
-    p = res;
-    // 	for(p = res;p != NULL; p = p->ai_next) {
-    void* addr;
-    //		char *ipver;
-
-    // get the pointer to the address itself,
-    // different fields in IPv4 and IPv6:
-    if (p->ai_family == AF_INET) {
-        struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
-        addr                     = &(ipv4->sin_addr);
-        strcpy(ip, inet_ntoa(ipv4->sin_addr));
-    } else {
-        // TODO - test
-        struct sockaddr_in6* ipv6 = (struct sockaddr_in6*)p->ai_addr;
-        addr                      = &(ipv6->sin6_addr);
-        // inet_ntop function does not exist on windows
-        // http://www.mail-archive.com/users@ipv6.org/msg02107.html
-        getnameinfo((struct sockaddr*)ipv6, sizeof(struct sockaddr_in6), ip, 1, NULL, 0, NI_NUMERICHOST);
-    }
-    freeaddrinfo(res); // free the linked list
-//	}
+    freeaddrinfo(res);
 #else
+    // 不支持IPv6的情况
     (void)domainName;
-    (void)ip;
-#endif // #if RAKNET_SUPPORT_IPV6==1
+    memset(ip, 0, 65);
+#endif
 }
 
 
